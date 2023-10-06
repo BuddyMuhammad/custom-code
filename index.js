6b8abeb2eb3ab1f05672b59eea5253bf0a9c36eb
GeeksEra <geeksera.online@gmail.com>
2023-10-06 15:53:59 +0400

    updates

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-export const applyCustomCode = (externalCodeSetup: any) => {
+
+import {
+	View,
+	Text,
+	TextInput
+} from "react-native"; export const applyCustomCode = (externalCodeSetup: any) => {
 	// call custom code api here
 
 	externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
-		<View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
 			<Text>Hello from custom Signup Screen</Text>
 			<TextInput placeholder={"login"} />
 			<TextInput placeholder={"password"} />
 		</View>
 	));
-	
+
 };
